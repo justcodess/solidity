fce8bff25144facf5ee69ab545b3420da85f84ef
justcodess <zeynepdeler@gmail.com>
2024-09-02 19:03:15 +0300

    commit

--- createToken.sol
+++ createToken.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.24;
 
 contract nsToken {
-   string public name= "NS Token";
+   string public name= "NS";
    string public symbol= "NS";
    uint8 public decimals = 18;
    uint256 public totalSupply = 1000000 * (10 ** 18);
